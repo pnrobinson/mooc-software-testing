feadc0746e3e5b37b13f037af0388b056574a0c8
pnrobinson <peter.robinson@jax.org>
2018-11-10 11:21:49 -0500

    gHappy

--- src/main/java/tudelft/ghappy/GHappy.java
+++ src/main/java/tudelft/ghappy/GHappy.java
@@ -4,9 +4,10 @@ public class GHappy {
 
     public boolean gHappy(String str) {
         assert str!=null;
+        if (str.isEmpty()) return true; // no g's in the string, so all g's are happy
         for(int i = 0; i < str.length(); i++) {
             if(str.charAt(i) == 'g') {
-                if (i >= 0 && str.charAt(i-1) == 'g') { continue; }
+                if (i > 0 && str.charAt(i-1) == 'g') { continue; }
                 if (i+1 < str.length() && str.charAt(i+1) == 'g') { continue; }
                 return false;
             }
--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,52 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+/*
+
+    gHappy("xxggxx") ==> true
+    gHappy("xxgxx") ==> false
+    gHappy("xxggyygxx") ==> false
+
+ */
+
+private  GHappy ghappy;
+
+@BeforeEach
+public  void init(){
+    ghappy = new GHappy();
+}
+    @Test
+    public void test1() {
+        boolean b = ghappy.gHappy("xxggxx");
+        Assertions.assertTrue(b);
+    }
+
+    @Test
+    public void testOnlyOneG() {
+    boolean b = ghappy.gHappy("xxgxx");
+    Assertions.assertFalse(b);
+    }
+
+    @Test
+    public void testMixedString() {
+        boolean b = ghappy.gHappy("xxggyygxx");
+        Assertions.assertFalse(b);
+    }
+
+    @Test
+    public void testSingleG() {
+        boolean b = ghappy.gHappy("g");
+        Assertions.assertFalse(b);
+    }
+
+    @Test
+    public void testEmptyString() {
+        boolean b = ghappy.gHappy("");
+        Assertions.assertTrue(b);
+    }
 }
